366e605af737f6554ce9edc5c2c90ef569c87f0d
alex <ovechko.056@gmail.com>
2024-05-25 18:49:36 +0200

    Demo app for iOS

---
+++ DemoApp/Test-iOS/Sources/ContentView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+
+    var body: some View {
+        VStack {
+            Text(textInfo())
+                .multilineTextAlignment(.center)
+        }
+        .padding(25)
+        .onAppear {
+            Task {
+                try await viewModel.test()
+            }
+        }
+    }
+    
+    private func textInfo() -> String {
+        switch viewModel.state {
+        case .initial:
+            return ""
+        case .loadingModel:
+            return "Loading ml model..."
+        case .loadingExampleTensors:
+            return "Loading example input and output tensors from json files..."
+        case .runningModel:
+            return "Running model..."
+        case .validation:
+            return "Comparing CoreML and PyTorch output tensors..."
+        case let .completed(ok):
+            let extraInfo = "\nFor more info please read the logs"
+            
+            if ok {
+                return "Validation: successful\n" +
+                       "(CoreML and PyTorch output tensors are equal)" +
+                        extraInfo
+            }
+            return "Validation: failed\n" +
+                   "(CoreML and PyTorch output tensors aren't equal)" +
+                   extraInfo
+        }
+    }
+}

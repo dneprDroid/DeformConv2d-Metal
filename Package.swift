1ad020ceb0b4669df3910f57670ec26b6d7b70c8
alex <ovechko.056@gmail.com>
2024-05-25 18:31:03 +0200

    Swift PM package

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "DeformConv2d-Metal",
+    platforms: [
+        .iOS(.v15),
+        .macOS("11.0")
+    ],
+    products: [
+        .library(
+            name: "DeformConv2dMetal",
+            type: .dynamic,
+            targets: ["DeformConv2dMetal"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "DeformConv2dMetal",
+            resources: [
+                .copy("Resources/Metal"),
+            ]
+        )
+    ]
+)
